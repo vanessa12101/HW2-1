f5d9f8ce92329c8708711fc1d7352a769c77d8b5
vanessa12101 <choumina838@gmail.com>
2022-11-17 23:36:08 +0800

    Initial Commit

---
+++ HW2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2-1
+//
+//  Created by 陳家堯 on 2022/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
